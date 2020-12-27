2551ae4e412dc65c91b069b4b513eb6269088c15
Thompson Lane <thompsonplane@gmail.com>
2020-12-26 23:45:22 -0600

    Finished Day 9.

---
+++ Day9/part1.nim
@@ -0,0 +1,46 @@
+import re
+import intsets
+import sequtils
+import strutils
+import tables
+
+var numbers: seq[int]
+
+let file = open("input.txt", FileMode.fmRead)
+var line: string
+while file.readLine(line):
+    numbers.add(parseInt(line))
+close(file)
+
+#[
+Find the first number in the list which
+is not the sum of two of the previous 25
+numbers in the list.
+Note: the two summed numbers must not be equal.
+]#
+
+# Preamble length is 25. Must include a number
+# after the preamble.
+var preambleLength = 25
+if len(numbers) < preambleLength + 1:
+    echo "Not enough numbers to form a preamble"
+    quit(1)
+
+var result = -1
+
+for i in countup(preambleLength, len(numbers) - 1, 1):
+    var num = numbers[i]
+    var preamble1 = numbers[(i - preambleLength)..(i - 1)].toIntSet()
+    var preamble2 = numbers[(i - preambleLength)..(i - 1)].toIntSet()
+
+    var sumFound = false
+    for j in preamble1:
+        for k in preamble2:
+            if j + k == num:
+                sumFound = true
+
+    if not sumFound:
+        result = num
+        break
+
+echo result
